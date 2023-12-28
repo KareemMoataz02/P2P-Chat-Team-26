f71aab1275c2cba952a648a0a4a6003f68bb2796
KareemMoataz02 <kareemmoataz13@gmail.com>
2023-12-28 12:02:18 +0200

    Update db.py
    Added Room functions

--- db.py
+++ db.py
@@ -1,31 +1,23 @@
 from pymongo import MongoClient
 import bcrypt
 
-# Includes database operations
+
 class DB:
-
-
-    # db initializations
     def __init__(self):
         self.client = MongoClient('mongodb://localhost:27017/')
         self.db = self.client['p2p-chat']
 
-
-    # checks if an account with the username exists
     def is_account_exist(self, username):
-        if len(list(self.db.accounts.find({'username': username}))) > 0:
-            return True
-        else:
-            return False
-    
+        return len(list(self.db.accounts.find({'username': username}))) > 0
 
-    # registers a user
     def register(self, username, password):
         if len(username) < 3:
             return "join-failed-username"
         if len(password) < 5:
             return "join-failed-password"
-        hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+        hashed_password = bcrypt.hashpw(
+            password.encode('utf-8'), bcrypt.gensalt())
         account = {
             "username": username,
             "password": hashed_password
@@ -33,22 +25,13 @@ class DB:
         self.db.accounts.insert_one(account)
         return "join-success"
 
-
-    # retrieves the password for a given username
     def get_password(self, username, password):
         hashed = self.db.accounts.find_one({"username": username})["password"]
         return bcrypt.checkpw(password.encode('utf-8'), hashed)
 
-
-    # checks if an account with the username online
     def is_account_online(self, username):
-        if len(list(self.db.online_peers.find({"username": username}))) > 0:
-            return True
-        else:
-            return False
+        return len(list(self.db.online_peers.find({"username": username}))) > 0
 
-    
-    # logs in the user
     def user_login(self, username, ip, port):
         online_peer = {
             "username": username,
@@ -56,19 +39,48 @@ class DB:
             "port": port
         }
         self.db.online_peers.insert_one(online_peer)
-    
 
-    # logs out the user 
     def user_logout(self, username):
         self.db.online_peers.delete_one({"username": username})
-    
 
-    # retrieves the ip address and the port number of the username
     def get_peer_ip_port(self, username):
         res = self.db.online_peers.find_one({"username": username})
         return (res["ip"], res["port"])
-    
+
     def is_address_connected(self, ip, port):
-        if len(list(self.db.online_peers.find({"ip": ip, "port": port}))) > 0:
-            return True
-        return False
+        return len(list(self.db.online_peers.find({"ip": ip, "port": port}))) > 0
+
+    def get_online_users(self):
+        projection = {'username': 1, '_id': 0}
+        return list(self.db.online_peers.find({}, projection))
+
+    def get_rooms(self):
+        projection = {'roomId': 1, '_id': 0}
+        return list(self.db.rooms.find({}, projection))
+
+    def is_room_exist(self, roomId):
+        return len(list(self.db.rooms.find({'roomId': roomId}))) > 0
+
+    def create_room(self, roomId):
+        if self.is_room_exist(roomId):
+            raise ValueError(f"Room with id {roomId} already exists.")
+
+        room = {
+            "roomId": roomId,
+            "peers": []
+        }
+        self.db.rooms.insert_one(room)
+
+    def get_room_users(self, roomId):
+        res = self.db.rooms.find_one({"roomId": roomId})
+        return res["_id"], res["peers"]
+
+    def update_room(self, roomId, peers):
+        projection = {"_id": roomId}
+        update_data = {"$set": {"peers": peers}}
+        self.db.rooms.update_one(projection, update_data)
+
+    def remove_user(self, roomId, peer):
+        projection = {"roomId": roomId}
+        update_data = {"$pull": {"peers": peer}}
+        self.db.rooms.update_one(projection, update_data)
